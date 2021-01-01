db92696cc33fade15fcccf092d53673a41b72309
Mike <jmbowlin13@gmail.com>
2020-12-31 23:58:07 -0700

    FDS-100: Style-Dictionary.

---
+++ style-dictionary/build/ios-swift/StyleDictionarySize.swift
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.swift
+//
+// Do not edit directly
+// Generated on Fri, 01 Jan 2021 06:45:32 GMT
+//
+
+import UIKit
+
+public enum StyleDictionarySize {
+    public static let fontBase = CGFloat(16.00)
+    public static let fontLarge = CGFloat(32.00)
+    public static let fontMedium = CGFloat(16.00)
+    public static let fontSmall = CGFloat(12.00)
+}
